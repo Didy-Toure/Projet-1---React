1112d3e2675ac2ba94dd295bfcf6c5448f7dbdc6
Natissa-DidyToure <natissadidy@gmail.com>
2023-06-29 16:25:19 -0400

    Creation du fichier jsx
    Il y a une erreur dans mon fichier App.js. en visionant mon app sur le navigateur. Jai un undefined error. J'ai un bug à fixer

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const PokemonCard = ({ name, image, abilities }) => {
+  const [showAbilities, setShowAbilities] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowAbilities(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowAbilities(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pokemon-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <h2>{name}</h2>
+      <img src={image} alt={name} />
+      {showAbilities && (
+        <ul>
+          {abilities.map((ability, index) => (
+            <li key={index}>{ability}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
+
+
+const App = () => {
+  const pokemonData = [
+    {
+      name: "Bulbasaur",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      abilities: [ 'overgrow', 'chlorophyll' ]
+    }
+  ];
+
+  return (
+    <div className="App">
+      <h1>Pokemon Cards</h1>
+      <div className="pokemon-container">
+        {pokemonData.map((pokemon, index) => (
+          <PokemonCard
+            key={index}
+            name={pokemon.name}
+            image={pokemon.image}
+            abilities={pokemon.abilities}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
